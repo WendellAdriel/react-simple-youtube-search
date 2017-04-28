24fb8f277aa5da11fc832b6bf069873594c0c84b
Wendell Adriel <wendelladriel.ti@gmail.com>
2017-04-28 14:13:21 +0100

    created other components to show video details and video list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
+import Config from './config/config'
 import SearchBar from './components/search-bar'
-import { API_KEY } from './config/config'
+import VideoList from './components/video-list'
+import VideoDetail from './components/video-detail'
 
-const App = () => (
-  <div>
-    <SearchBar />
-  </div>
-)
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
+
+    YTSearch({ key: Config.API_KEY, term: 'Dio full concert' }, videos => this.setState({ videos, selectedVideo: videos[0] }))
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={ this.state.selectedVideo } />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
+          videos={ this.state.videos } />
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.app'))
